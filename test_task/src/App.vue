3da2df6a28a84d6e172bfadb7f771ab21fadb497
Kurosage <asuna35981@gmail.com>
2024-10-26 18:35:00 +0300

    testZadanie

---
+++ test_task/src/App.vue
@@ -0,0 +1,52 @@
+
+
+<template>
+
+
+  <main class="main">
+      <p class="main__text_save-process" v-if="saveProcessing">Информация сохраняется</p>
+      <my-form :item="item" @save="v => saveHandler(v)" :disabled="saveProcessing"></my-form>
+  </main>
+</template>
+<script setup>
+import MyForm from './components/my-form.vue'
+import { ref } from 'vue';
+const item = ref({"name":"Иван",age:10})
+const newItem = ref(null)
+const saveProcessing = ref(false)
+
+async function saveHandler(value){
+  saveProcessing.value = true
+  try{
+ 
+    await new Promise((resolve)=>{setTimeout(resolve,5000)})
+  }catch(error){
+    console.log( error)
+  }finally{
+    
+    saveProcessing.value = false
+  }
+  
+
+  
+  newItem.value = value
+}
+</script>
+
+<style scoped>
+.main{
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.main__text_save-process{
+  font-size: bold;
+  color:red;
+ 
+  
+  font-size: 25px;
+  margin:  25px;
+}
+</style>
---
+++ test_task/src/components/my-form.vue
@@ -0,0 +1,108 @@
+
+
+<template>
+
+    <div class="form" >
+        <label class="form__label label label_name" for="input_name">Имя</label>
+        <p v-if="isInvalidName" class="form__error error error_name">Поле не может быть пустым</p>
+        <input class="form__input input input_name" type="text" v-model="name" placeholder="Введите имя" min-length="1" :disabled="disabled" required>
+        
+        <label class="form__label label label_age" for="input_age">Возраст</label>
+        <p v-if="isInvalidAge" class="form__error error error_age">Введите возраст от 18 до 100 лет</p>
+        <input class="form__input input input_age" type="number" v-model="age" placeholder="Введите возраст" :disabled="disabled" required>
+        
+        <button class="form__button button button_save" @click="saveInfo" :disabled="disabled">Сохранить</button>
+        <button class="form__button button button_reset" @click="resetInfo" :disabled="disabled">Отменить</button>
+    </div>
+    
+</template>
+<script setup>
+    import { defineProps } from 'vue';
+    import { ref } from 'vue';
+    import { defineEmits } from 'vue';
+    const isInvalidName = ref(false)
+    const isInvalidAge = ref(false)
+    const props = defineProps({
+        item:{
+            type:Object,
+            required:true
+        },
+        disabled:{
+            type:Boolean,
+            required:true
+        }
+    })
+    const emit = defineEmits(["save"])
+    const name = ref(props.item.name?props.item.name:"")
+    const age = ref(props.item.age?props.item.age:"")
+    function resetInfo(){
+        name.value = props.item.name
+        age.value = props.item.age
+        isInvalidName.value = false
+        isInvalidAge.value = false
+    }
+    function saveInfo(){
+        itemValidation()
+        if (!isInvalidName.value && !isInvalidAge.value ){
+            isInvalidName.value = false
+            isInvalidAge.value = false
+            emit('save',{name: name.value,age: age.value})
+        }
+    }
+    function itemValidation(){
+        if (name.value.trim().length != 0){
+            isInvalidName.value  = false
+           
+        }else{
+            isInvalidName.value  = true
+           
+        }
+        if (age.value >= 18 && age.value <=100){
+            isInvalidAge.value = false
+           
+        }else{
+            isInvalidAge.value = true
+        }
+    }
+   
+    
+</script>
+<style scoped>
+.form{
+    background-color: rgb(233, 159, 171);
+    border-radius: 5px;
+    padding: 50px 5% ;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap:10px;
+}
+.input{
+    height: 20px;
+    border-radius:inherit;
+    border: 0;
+}
+.button{
+    border-radius:inherit;
+    height: 20px;
+    width: 50%;
+    font-weight: bold;
+    border: 0;
+}
+.button_save{
+    background-color: rgb(54, 172, 64);
+}
+.button_reset{
+    background-color: rgb(233, 85, 85);
+}
+.label{
+    font-size: 20px;
+    font-weight: bold;
+}
+.form__error{
+    color:red;
+    padding: 0;
+    margin: 0;
+}
+</style>
+  
